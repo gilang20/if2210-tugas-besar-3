184468b583c069c2a40df409ce0375e44eb6c54c
Adya Naufal Fikri <13515130@std.stei.itb.ac.id>
2017-04-25 02:36:22 +0700

    score panel update

--- src/view/ScorePanel.java
+++ src/view/ScorePanel.java
@@ -6,18 +6,24 @@
 
 package view;
 
+import controller.DataController;
+import java.io.FileNotFoundException;
 /**
  * Panel skor.
  * Menampilkan skor permainan.
  * @author Asus
  */
 public class ScorePanel extends javax.swing.JPanel {
-
+  /*Controller untuk player*/
+  private DataController dataController;
+  
   /**
    * Creates new form ScorePanel
    */
-  public ScorePanel() {
+  public ScorePanel() throws FileNotFoundException{
     initComponents();
+    dataController = new DataController(score, highScore,level);
+    dataController.start();
   }
 
   /**
@@ -29,23 +35,55 @@ public class ScorePanel extends javax.swing.JPanel {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
+    score = new javax.swing.JLabel();
+    highScore = new javax.swing.JLabel();
+    level = new javax.swing.JLabel();
+
     setBackground(new java.awt.Color(153, 153, 153));
     setMaximumSize(new java.awt.Dimension(714, 50));
     setMinimumSize(new java.awt.Dimension(714, 50));
+
+    score.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+    score.setForeground(new java.awt.Color(0, 0, 0));
+    score.setText("Score : ");
+
+    highScore.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+    highScore.setForeground(new java.awt.Color(0, 0, 0));
+    highScore.setText("High Score : ");
+
+    level.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+    level.setForeground(new java.awt.Color(0, 0, 0));
+    level.setText("Level : ");
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
     this.setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 714, Short.MAX_VALUE)
+      .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+        .addContainerGap(342, Short.MAX_VALUE)
+        .addComponent(level)
+        .addGap(55, 55, 55)
+        .addComponent(highScore)
+        .addGap(70, 70, 70)
+        .addComponent(score)
+        .addGap(54, 54, 54))
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 50, Short.MAX_VALUE)
+      .addGroup(layout.createSequentialGroup()
+        .addGap(15, 15, 15)
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+          .addComponent(score)
+          .addComponent(highScore)
+          .addComponent(level))
+        .addContainerGap(16, Short.MAX_VALUE))
     );
   }// </editor-fold>//GEN-END:initComponents
 
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JLabel highScore;
+  private javax.swing.JLabel level;
+  private javax.swing.JLabel score;
   // End of variables declaration//GEN-END:variables
 }
